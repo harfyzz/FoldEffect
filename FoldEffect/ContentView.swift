7842469f60c579ab851bf9658c33e85477220378
Afeez <harfyzz@gmail.com>
2025-04-02 17:28:23 +0100

    Initial Commit

---
+++ FoldEffect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoldEffect
+//
+//  Created by Afeez Yunus on 02/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoldEffect/FoldEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoldEffectApp.swift
+//  FoldEffect
+//
+//  Created by Afeez Yunus on 02/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct FoldEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
